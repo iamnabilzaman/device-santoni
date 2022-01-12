1cf6bc13a58c21d30f6031d587fa216eda9c48e8
iamnabilzaman <94600437+iamnabilzaman@users.noreply.github.com>
2022-01-13 01:43:18 +0600

    santoni: Added RohieOS support

--- aosp_santoni.mk
+++ aosp_santoni.mk
@@ -1,6 +1,6 @@
 # Inherit makefiles
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # ROM spesific makefile name
 PRODUCT_NAME := aosp_santoni
